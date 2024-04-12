b32975b6269b22903b94098bcd34f78140e1c57b
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-04-12 20:05:59 +0300

    made changes

--- heaps.py
+++ heaps.py
@@ -1,8 +1,18 @@
 import heapq
-Nums=[50,15,5,20,70]
-heapq.heappush(Nums,18)
-print(Nums)
+#min heap
+Nums=[50,10,5,20,70,45,67,89]
 heapq.heapify(Nums)
+heapq.heappush(Nums,23)
+heapq.heappush(Nums,15)
+heapq.heappush(Nums,43)
 print(Nums)
 heapq.heappop(Nums)
 print(Nums)
+heapq.heapreplace(Nums,9)
+heapq.heapreplace(Nums,19)
+print(Nums)
+Nums.remove(70)
+print(Nums)
+Nums.remove(4)
+print(Nums)
+
